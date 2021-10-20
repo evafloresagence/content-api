42fd4de67ad1ea32006a7a4cd9c0503a140dde98
Alejandro González <alejandroga@aica.cu>
2021-10-20 14:25:09 -0400

    fix

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST'),
+        host: env('DATABASE_HOST', '10.25.27.74'),
         port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
