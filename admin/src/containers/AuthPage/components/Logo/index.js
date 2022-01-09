130b891c3744c2dd308d5f45cfad7804282f2222
Alejandro González <alejandroga@aica.cu>
2022-01-09 14:46:57 -0500

    strapi customization

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoStrapi from '../../../../assets/images/aica-header.png';
+import Img from './Img';
+
+const Logo = () => <Img src={LogoStrapi} alt="strapi-logo" />;
+
+export default Logo;
