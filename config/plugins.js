58ec2a7439133d783dc0be75edf10964930d74de
Alejandro González <alejandroga@aica.cu>
2022-01-19 16:55:27 -0500

    fix minio

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       accessKey: env("MINIO_ACCESS_KEY", 'admin'),
       secretKey: env("MINIO_SECRET_KEY", 'Desarrollo'),
       bucket: env("MINIO_BUCKET", 'content-api'),
-      endPoint: env("MINIO_ENDPOINT", '10.25.27.74'),
+      endPoint: env("MINIO_ENDPOINT", '192.168.208.86'),
       port: parseInt(env("MINIO_PORT", '9020'), 10),
       useSSL: env("MINIO_USE_SSL", false),
       folder: "upload",
