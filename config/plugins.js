029d1f95fe3df08ad9750e7be172df6b4f2ec4d6
Alejandro González <alejandroga@aica.cu>
2022-02-01 12:31:55 -0500

    change s3 host

--- config/plugins.js
+++ config/plugins.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       accessKey: env("MINIO_ACCESS_KEY", 'admin'),
       secretKey: env("MINIO_SECRET_KEY", 'Desarrollo'),
       bucket: env("MINIO_BUCKET", 'content-api'),
-      endPoint: env("MINIO_ENDPOINT", '192.168.208.86'),
+      endPoint: env("MINIO_ENDPOINT", '192.168.205.249'),
       port: parseInt(env("MINIO_PORT", '9020'), 10),
       useSSL: env("MINIO_USE_SSL", false),
       folder: "upload",
       isDocker: true,
-      host: env("MINIO_HOST", '192.168.208.86')
+      host: env("MINIO_HOST", '192.168.205.249')
     }
   }
 });
