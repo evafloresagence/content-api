2ab712d0ab16ff9ef9fdf6958e20ff8f15a97e17
Alejandro Gonzalez <alegonzo97@gmail.com>
2023-06-08 15:50:29 -0400

    change minio provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+/*module.exports = ({ env }) => ({
   upload: {
     provider: "tp-minio",
     providerOptions: {
@@ -13,4 +13,20 @@ module.exports = ({ env }) => ({
       host: env("MINIO_HOST", 'marketing.aica.cu')
     }
   }
+});*/
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'minio-ce',
+    providerOptions: {
+      accessKey: env("MINIO_ACCESS_KEY", 'admin'),
+      secretKey: env("MINIO_SECRET_KEY", 'Desarrollo'),
+      bucket: env("MINIO_BUCKET", 'content-api'),
+      endPoint: env("MINIO_ENDPOINT", '192.168.205.249'),
+      port: parseInt(env("MINIO_PORT", '9020'), 10),
+      useSSL: env("MINIO_USE_SSL", false),
+      folder: "upload",
+      isDocker: true,
+      host: env("MINIO_HOST", 'marketing.aica.cu')
+    }
+  },
 });
